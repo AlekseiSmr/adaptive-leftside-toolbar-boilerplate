4c0aa07d18e9d4b9cb7fac3e115c305bd2903c42
Aleksei Ermolov <aleksei_ermolov@epam.com>
2019-05-31 09:06:17 +0400

    master: cleanup code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,27 +17,6 @@ export const animation = trigger('slideInOut', [
   ]),
 ]);
 
-export const minimalAnimation = trigger('minimal', [
-  state(
-    'open',
-    style({
-      height: '200px',
-      opacity: 1,
-      backgroundColor: 'yellow',
-    })
-  ),
-  state(
-    'closed',
-    style({
-      height: '100px',
-      opacity: 0.5,
-      backgroundColor: 'green',
-    })
-  ),
-  transition('open => closed', [animate('1s')]),
-  transition('closed => open', [animate('0.5s')]),
-]);
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
